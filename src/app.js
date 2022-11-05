134fc13f0dabe827143a1c49bd9a0496ac97b3d9
ayon <ashiqulislamayon28@gmai.com>
2022-11-05 15:28:22 +0600

    little changes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require("path")
 const app = express();
+const hbs = require('hbs')
 const PORT = process.env.PORT || 3000;
 
 const uri = "mongodb+srv://ayon:400220821fjw0676@cluster0.zkeepbh.mongodb.net/GAI?retryWrites=true&w=majority"
@@ -18,8 +19,13 @@ async function connect(){
 connect()
 
 const static_path = path.join(__dirname, "../public");
-app.use(express.static(static_path))
-app.set("view engine", "hbs")
+const templates_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
+app.use(express.static(static_path));
+app.set("view engine", "hbs");
+app.set("views", templates_path);
+hbs.registerPartials(partials_path);
+
 app.get('/', (req, res) => {
     res.render('index')
 });
