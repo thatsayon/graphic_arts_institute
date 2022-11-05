10f57cbc1ea0c6ec8828f77fa8966e910632342b
ayon <ashiqulislamayon28@gmai.com>
2022-11-05 16:28:31 +0600

    Done

--- src/app.js
+++ src/app.js
@@ -3,15 +3,16 @@ const mongoose = require('mongoose');
 const path = require("path")
 const app = express();
 const hbs = require('hbs')
+const Register = require("./models/registers")
 const PORT = process.env.PORT || 3000;
 
 const uri = "mongodb+srv://ayon:400220821fjw0676@cluster0.zkeepbh.mongodb.net/GAI?retryWrites=true&w=majority"
 
-async function connect(){
-    try{
+async function connect() {
+    try {
         await mongoose.connect(uri)
         console.log("Connected to MongoDB")
-    } catch(err){
+    } catch (err) {
         console.error(err)
     }
 }
@@ -21,6 +22,9 @@ connect()
 const static_path = path.join(__dirname, "../public");
 const templates_path = path.join(__dirname, "../templates/views");
 const partials_path = path.join(__dirname, "../templates/partials");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(static_path));
 app.set("view engine", "hbs");
 app.set("views", templates_path);
@@ -30,6 +34,23 @@ app.get('/', (req, res) => {
     res.render('index')
 });
 
+app.get('/confirm', (req, res) => {
+    res.send("Confirm")
+});
+
+app.post('/confirm', async (req, res) => {
+    try {
+        const registerUser = new Register({
+            firstname : req.body.firstname,
+            lastname : req.body.lastname
+        })
+
+        const register = await registerUser.save()
+        res.status(201).render("index");
+    } catch (error) {
+        res.status(400).send(error);
+    }
+})
 app.listen(PORT, () => {
     console.log(`The server is running on ${PORT}`)
 })
---
+++ src/models/registers.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstname : {
+        type:String, 
+        required:true
+    },
+    lastname : {
+        type:String,
+        required:true
+    }
+})
+
+const Register = new mongoose.model("Register", userSchema);
+
+module.exports = Register;
